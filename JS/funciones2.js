eb8ca89fa2fea169908f8e19a3bbd7ed17e662fd
Jose Luis <juss.jlgm@gmail.com>
2022-07-22 22:53:55 -0300

    ejercicio 2 function resuelto

---
+++ JS/funciones2.js
@@ -0,0 +1,23 @@
+//2- Definir una función que muestre información sobre una cadena de texto que se le pasa como argumento. 
+//A partir de la cadena que se le pasa, la función determina si esa cadena está formada sólo por mayúsculas, sólo por minúsculas o por una mezcla de ambas.
+
+function info(cadena) {
+
+    let resultado = "<br>La cadena \""+cadena+"\" ";
+  
+    // Comprobar mayúsculas y minúsculas
+    if(cadena == cadena.toUpperCase()) {
+      resultado += " está formada sólo por mayúsculas";
+    }
+    else if(cadena == cadena.toLowerCase()) {
+      resultado += " está formada sólo por minúsculas";
+    }
+    else {
+      resultado += " está formada por mayúsculas y minúsculas";
+    }
+  
+    return resultado;
+  }
+  document.write(info("EL AUTO SIRVE PARA TRANSPORTARSE"));
+document.write(info("el combustible subió de valor..."));
+document.write(info("Los autos están caRos"));
