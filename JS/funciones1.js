34cc2a2bdfb1e14bdb3947d5c57101c6e036f271
Jose Luis <juss.jlgm@gmail.com>
2022-07-22 21:51:40 -0300

    ejercicio 1 function resuelto

---
+++ JS/funciones1.js
@@ -0,0 +1,20 @@
+
+//1- Escribir el código de una función a la que se pasa como parámetro un número entero  
+//y devuelve como resultado una cadena de texto que indica si el número es par o impar.
+//Mostrar por pantalla el resultado devuelto por la función.
+
+
+function parImpar(numero){
+    if(numero % 2 == 0){
+        return 'par';
+    }
+    else{
+        return 'impar';
+    }
+}
+
+    
+    
+
+
+
